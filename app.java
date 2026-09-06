15198b990a6b87609ad9c747a0b418b695f451e4
Manikanta <154468108+manikanta0814@users.noreply.github.com>
2026-09-06 17:38:35 +0530

    Update app.java

--- app.java
+++ app.java
@@ -2,7 +2,7 @@ public class Main {
    public static void main(String[] args) {
        System.out.println("Hello World ");
       system.out.println("hai ra babu");
-      system.out.println("hai");
+      system.out.println("hejkfbewai");
       
    }
 }
