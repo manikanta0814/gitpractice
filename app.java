d18f9c4ea5d77fa4c49e39d6d40601fe3e520074
Manikanta <154468108+manikanta0814@users.noreply.github.com>
2026-09-06 14:01:22 +0530

    Fix print statements in app.java

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public class Main {
    public static void main(String[] args) {
-       System.out.println("Hello World jhgfdcytxcjg");
+       System.out.println("Hello World ");
+      system.out.println("hai ra kanna");
    }
 }
